8bb0f893592c053fbe99d8ee2317ba5d02282929
KlavierCat <plin@tcd.ie>
2017-02-25 18:31:52 +0000

    solution 4 for challenge 1 population very clever

--- 01-population.js
+++ 01-population.js
@@ -1,10 +1,4 @@
-function nbYear(p0, percent, aug, p) {
-    var year;
-
-    for (year = 0; p0 < p; year++) {
-      p0 += p0 * (percent/100) + aug;
-    }
-
-    return year;
+function nbYear(p0, percent, aug, p, years = 0) {
+  return (p0 < p) ? nbYear(p0 * (1 + percent/100) + aug, percent, aug, p, years + 1) : years;
 }
-// Call to nbYear(1500, 5, 100, 5000) took 0.1849999998230487 milliseconds.
+// Call to nbYear(1500, 5, 100, 5000) took 7.629999999888241 milliseconds.
