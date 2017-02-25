c9a970767c4d3001294c97be236e065a6127bb19
KlavierCat <plin@tcd.ie>
2017-02-25 18:11:16 +0000

    solution 2 for challenge 1 population greatly improves performance

--- 01-population.js
+++ 01-population.js
@@ -1,11 +1,10 @@
 function nbYear(p0, percent, aug, p) {
-     var year = 1;
+    var year;
 
-     while ((p0 * (1 + percent/100) + aug) < p) {
-         p0 = p0 * (1 + percent/100) + aug;
-         year += 1;
-     }
+    for (year = 0; p0 < p; year++) {
+      p0 = p0 * (1 + percent/100) + aug;
+    }
 
-     return year;
+    return year;
 }
-// Call to nbYear(1500, 5, 100, 5000) took 8.234999999869615 milliseconds.
+// Call to nbYear(1500, 5, 100, 5000) took 0.12000000011175871 milliseconds.
