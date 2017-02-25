253947efbc12bcde861dc33574a3b7dc71e8afc8
KlavierCat <plin@tcd.ie>
2017-02-25 18:16:23 +0000

    solution 3 for challenge 1 population took slightly longer

--- 01-population.js
+++ 01-population.js
@@ -2,9 +2,9 @@ function nbYear(p0, percent, aug, p) {
     var year;
 
     for (year = 0; p0 < p; year++) {
-      p0 = p0 * (1 + percent/100) + aug;
+      p0 += p0 * (percent/100) + aug;
     }
 
     return year;
 }
-// Call to nbYear(1500, 5, 100, 5000) took 0.12000000011175871 milliseconds.
+// Call to nbYear(1500, 5, 100, 5000) took 0.1849999998230487 milliseconds.
